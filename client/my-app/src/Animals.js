3bc4b91a4ad81bcc168a2fe0fd98738bcda8870e
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-01-06 18:26:17 -0500

    search bar almost done

--- client/my-app/src/Animals.js
+++ client/my-app/src/Animals.js
@@ -5,9 +5,10 @@ class Animals extends Component{
     super(props);
     this.state = {
         allanimals: [],
-        filter: ""
+        searchedAnimal: ""
         }
     }
+    
 
     componentDidMount(){
         fetch('http://localhost:3000/animals')
@@ -18,28 +19,42 @@ class Animals extends Component{
             })
         })
     }
-
+  
     handleChange = (event) => {
-        this.setState({filter: event.target.value});
-      }
-        
+        this.setState({searchedAnimal: event.target.value});
+       
+    }
+
+    handleSearch = () => {
+        console.log(this.state.allanimals.filter(obj => obj.name === this.state.searchedAnimal))
+    }
+
+    
+    
     render(){
         let FetchAnimal = this.state.allanimals.map(animal =>
-            <div>
-                <ul>
+            <div key={animal.id}>
+                <ul className="myUl">
                     <li className="liName">{animal.name}</li>
                     <li className="liGroup"> {animal.group}</li>
                 </ul>
             </div>)
-    
-                return(
+     
+     return(
             <div>
+            <button onClick={this.handleSearch}>a</button>
                 <h1>Animal Filter</h1>
+                
                 <h2>type in the name of an animal. Get the scientific group name back!</h2>
+                
                 <input type="text" name="name" id="myInput" onChange={this.handleChange}/>
+                
                 <p id="searched"></p>
+        
                {FetchAnimal}
-               {this.state.filter}
+              
+               {this.state.searchedAnimal}
+
             </div>
         )
     }
@@ -52,5 +67,9 @@ class Animals extends Component{
 
 export default Animals
 
+
 // if the filter state object is equal to the name attribute of any allanimals object
 // return the corresponding allanimals objects group 
+
+//if the text of the input is equal to the name attribute of any allanimals object
+//return that allanimals objects group
