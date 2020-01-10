4259a306d06059e22e3c70c8bbc979ada0fb8a09
georuiz817 <49955820+georuiz817@users.noreply.github.com>
2020-01-10 16:15:27 -0500

    styling with css, added image

--- client/my-app/src/Animals.js
+++ client/my-app/src/Animals.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react'
+ 
 
 class Animals extends Component{
     constructor(props) {
@@ -29,26 +30,24 @@ class Animals extends Component{
     let grabbedAnimal = (this.state.allanimals.filter(obj => obj.name === this.state.searchedAnimal)) // returns the whole object if match
     let grabbedGroup = (grabbedAnimal.map(object => object.group))
     document.getElementById("searched").innerHTML = grabbedGroup
-}
+   }
 
     placeLooper = () =>{
         var placeholders = ['bees','gorillas','zebras','lions', 'chickens','alligators'];
         var length  = placeholders.length;
         var counter = 0;
-    /*Store the object to a variable*/
         var inquire = document.getElementById('myInput');
 
-        function ChangePlaceholder(){
-    /*Compare with placeholders length*/
-         if(counter>=length){
-         counter=0;
+    function ChangePlaceholder(){
+        if(counter>=length){
+        counter=0;
         }
-    /*Update placeholder text*/
+   
         inquire.setAttribute('placeholder',placeholders[counter]);
-    /*Update counter as Index*/
-     counter++;
+ 
+        counter++;
     }
-    /*Call ChangePlaceholder() function after 1 seconds, [1000 millisecond = 1 second]*/
+  
     setInterval(ChangePlaceholder,1000);
     }
 
@@ -57,15 +56,15 @@ class Animals extends Component{
       
      return(
             <div>
-                <h1 id="header">Animal Filter</h1>
+                <h1 id="header">Animal-Search</h1>
                
                 <input type="text" placeholder="oysters" name="name" id="myInput" onChange={this.handleChange}/>
                
                 <button id="button" onClick={this.handleSearch}>Search</button>
                
                 <p id="searched"></p>
-        
 
+              
                </div>
         )
     }
